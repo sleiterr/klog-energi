7698fba4533fc335ebc56e3a653edfc67bd2f48a
Oleg <sleite4r@gmail.com>
2025-08-19 13:19:41 +0200

    resize img_cover

--- src/app/components/Featured/Featured.tsx
+++ src/app/components/Featured/Featured.tsx
@@ -15,7 +15,7 @@ import {
 
 const Featured = () => {
   return (
-    <section className="bg-debate mx-4 rounded-2xl mb-[120px]">
+    <section className="bg-debate mx-4 rounded-2xl">
       <div className="py-[120px] mx-auto px-50 max-w-5x1">
         <h2 className="font-semibold text-6xl text-debaten pb-[42px]">
           I Nordvestjylland har vi
